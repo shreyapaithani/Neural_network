48e947a4035a0fcd28be0f6fc2707c6e1eeef226
kanikatiwari0912 <kanikat920@gmail.com>
2025-05-01 22:05:08 +0530

    Update neural.py

--- neural.py
+++ neural.py
@@ -9,7 +9,11 @@ X_test=X_test.reshape(X_test[0],-1)/255.0
 print("Training :{X_train.shape}")
 print("Testing:{X_test.shape})
 input_size=784
-w1=np.random.randn(input_size,128)*0.01
-b1=np.zeros((1,128))
-print("input layer done")
+hidden_size=128;
+output_size=10;
+num_hiddenlayer=16
+np.random.seed(42)
+parameters['w1']=np.random.randn(input_size,hidden_size)*0.01
+parameters['b1']=np.zeros((1,hidden_size))
+
 
